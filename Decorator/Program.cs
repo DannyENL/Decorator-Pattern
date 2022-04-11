c048ffe06ce85f766150ad481b953bad49daf449
Danny <danny.edwards777@gmail.com>
2022-04-11 10:16:24 +0100

    Decorator design pattern

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -13,12 +13,12 @@ namespace Decorator
 
             Cake cake2 = new Cupcake(); //Create a new Cupcake object
             cake2 = new Sprinkles(cake2); //Use the Sprinkles decorator
-            Console.WriteLine(cake2.Description + " : " + cake2.Calories() + " calories"); //Display calorie count
+            Console.WriteLine(cake2.Description + " (" + cake2.Calories() + " calories)"); //Display calorie count
 
             Cake cake3 = new Sponge(); //Create a new Sponge object
             cake3 = new Almonds(cake3); //Use the Almonds decorator
             cake3 = new Strawberry(cake3); //Use the Strawberry decorator
-            Console.WriteLine(cake3.Description + " : " + cake3.Calories() + " calories"); //Display calorie count
+            Console.WriteLine(cake3.Description + " (" + cake3.Calories() + " calories)"); //Display calorie count
 
         }
     }
